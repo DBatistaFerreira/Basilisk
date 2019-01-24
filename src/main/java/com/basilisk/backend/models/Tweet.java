aa8b6176cb2f14818d2b33b890c37d1b8f16a74c
Daniel Batista-Ferreira <danielbatfer@hotmail.com>
2019-01-24 00:26:55 -0500

    BASK18    -Added @Column to all non-object fields in the entity classes    -Added Getters and Setters to each entity class    -Added @OnetoOne(cascade = CascadeType.ALL, targetEntity = User.class) and @JoinColumn(name="NAME") to the object fields    -Removed temp package files

--- src/main/java/com/basilisk/backend/models/Tweet.java
+++ src/main/java/com/basilisk/backend/models/Tweet.java
@@ -1,27 +1,71 @@
 package com.basilisk.backend.models;
 
-import org.springframework.data.annotation.Id;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 
 @Entity
 public class Tweet
 {
     @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.TABLE)
     private long id;
+
+    @Column
     private String text;
+
+    @Column
     private int likes;
+
+    @Column
     private int retweets;
+
+    @OneToOne(cascade = CascadeType.ALL, targetEntity = User.class)
+    @JoinColumn(name="USER")
     private User user;
+
+    public Tweet(){}
 
     public Tweet(String text, int likes, int retweets, User user)
     {
         this.text = text;
         this.likes = likes;
         this.retweets = retweets;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getRetweets() {
+        return retweets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public void setRetweets(int retweets) {
+        this.retweets = retweets;
+    }
+
+    public void setUser(User user) {
         this.user = user;
     }
 }
