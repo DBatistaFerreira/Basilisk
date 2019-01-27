46aab05d33e29b93b846319c93794526afee5dee
Vinh <congvinhvu@homail.com>
2019-01-27 16:01:07 -0500

    BASK6 (Issue #9): -Prototype home page view -Added vertical scroll bar -Limited posts to 140 chars -Posts are now red only -Adjusted the search bar to be horizontal -Added like, retweet, and comment button

--- src/main/java/com/basilisk/frontend/views/HomePageView.java
+++ src/main/java/com/basilisk/frontend/views/HomePageView.java
@@ -2,6 +2,7 @@ package com.basilisk.frontend.views;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
@@ -10,39 +11,79 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("Home")
+
 public class HomePageView extends VerticalLayout {
-    Integer textAreaCounter = 5;
+    Integer textAreaCounter = 10;
     String hexTurquoise = "#40E0D0";
 
+    private Tab home;
+    private Tab profile;
+    private Tab logout;
+    private Tabs tabs;
+
+    private HorizontalLayout searchBar;
+    private TextField searchTxt;
+    private Button searchButton;
+
+    String fill140Char() {
+        String oString = "Char140";
+        for (int i = 0; i < 140; ++i) {
+            oString += "a";
+        }
+        return oString;
+    }
+
     public HomePageView() {
+        //Scroll bar
+        this.setSizeUndefined();
+
+        //Vertical layout in teh center
+        this.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+
         //Tabs to navigate
-        Tab home = new Tab("Home");
-        Tab profile = new Tab("Profile");
+        home = new Tab("Home");
+        profile = new Tab("Profile");
+        logout = new Tab("Logout");
         home.getStyle().set("border", hexTurquoise);
         profile.getStyle().set("border", hexTurquoise);
-        Tabs tabs = new Tabs(home, profile);
+        logout.getStyle().set("border", hexTurquoise);
+
+        tabs = new Tabs(home, profile, logout);
         tabs.setFlexGrowForEnclosedTabs(1);
+
         add(tabs);
 
-        this.getStyle().set("border", hexTurquoise);
-        this.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
-
         //Search bar
-        TextField textField = new TextField();
-        textField.setLabel("Search");
-        Button searchBar = new Button("Search");
+        searchTxt = new TextField();
+        searchTxt.setPlaceholder("Search");
+        searchButton = new Button("Search");
 
-        add(textField, searchBar);
+        searchBar = new HorizontalLayout();
+        searchBar.add(searchTxt, searchButton);
+        searchBar.setPadding(false);
+        searchBar.setMargin(true);
+        searchBar.setSpacing(true);
+        add(searchBar);
 
-        //Messages or tweets
-        this.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        //Tweets or posts
+        String userName = "Username ";
         for (int i = 0; i < textAreaCounter; ++i) {
-            TextArea area = new TextArea("Username");
-            //area.setWordWrap(true);
-            area.setValue("Message");
+            userName += i;
 
-            add(area);
-            ++i;
+            TextArea area = new TextArea();
+            area.setReadOnly(true);
+            area.setWidth("300px");
+            area.setMaxLength(140 + userName.length());
+
+            area.setValue(userName + "\n \n" + fill140Char());
+
+            HorizontalLayout interactButtons = new HorizontalLayout();
+            Button likeButton = new Button("Like");
+            Button retweetButton = new Button("Retweet");
+            Button commentButton = new Button("Comment");
+            interactButtons.add(likeButton, retweetButton, commentButton);
+
+            add(area, interactButtons);
         }
     }
 }
