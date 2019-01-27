48ae93182d4ef16e9a933fcb7e16304f57e20803
Greg <cao.gregory@gmail.com>
2019-01-26 20:15:12 -0500

    #16: BASK13   -created create method in FriendList Service   -created update method in FriendList Service   -created retrieve method in FriendList Service   -created delete method in FriendList Service   -Autowired the FriendList repository to FriendListService

--- src/main/java/com/basilisk/backend/services/FriendListService.java
+++ src/main/java/com/basilisk/backend/services/FriendListService.java
@@ -1,8 +1,39 @@
 package com.basilisk.backend.services;
 
+import com.basilisk.backend.models.FriendList;
+import com.basilisk.backend.models.User;
+import com.basilisk.backend.repositories.FriendListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FriendListService {
+
+    private FriendListRepository friendListRepository;
+
+    @Autowired
+    public FriendListService(FriendListRepository friendListRepository) {
+        this.friendListRepository = friendListRepository;
+    }
+
+    public void createFriendList(FriendList friendList) {
+        friendListRepository.save(friendList);
+    }
+
+    public FriendList retrieveFriendListByUser(User user) {
+        return friendListRepository.findByUser(user);
+    }
+
+    public FriendList retrieveFriendList(long id) {
+        return friendListRepository.getOne(id);
+    }
+
+    public void updateFriendList(FriendList friendList) {
+        friendListRepository.save(friendList);
+    }
+
+    public void deleteFriendList(FriendList friendList) {
+        friendListRepository.delete(friendList);
+    }
 
 }
