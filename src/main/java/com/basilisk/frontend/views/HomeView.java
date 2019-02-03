db474b6e1b204b06556ab9aba21eab48786c49cc
Clearedkinkajou <bedardjake@gmail.com>
2019-02-02 22:34:26 -0500

    BASK33(Issue #56):
      -Added home-view.html
      -Added new vaadin tags
      -Added @Service Tag to HomePresenter

---
+++ src/main/java/com/basilisk/frontend/views/HomeView.java
@@ -0,0 +1,25 @@
+package com.basilisk.frontend.views;
+
+import com.basilisk.backend.presenters.HomePresenter;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.templatemodel.TemplateModel;
+
+@Tag("home-view")
+@HtmlImport("home-view.html")
+@Route("home")
+public class HomeView extends PolymerTemplate<HomeView.HomeViewModel> {
+
+    private HomePresenter homePresenter;
+
+    public HomeView(HomePresenter homePresenter) {
+        this.homePresenter = homePresenter;
+        // You can initialise any data required for the connected UI components here.
+    }
+
+    public interface HomeViewModel extends TemplateModel {
+        // Add setters and getters for template properties here.
+    }
+}
