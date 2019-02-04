53a89bca310e13e957a378f9c0d4e97160a23af0
najimgh <najimg1@hotmail.com>
2019-02-04 12:03:56 -0500

    BASK31 (Issue#55):    -Removed unnecessary objects from the LoginView. Only kept those that need listeners (signUpButton, loginButton, username textfield and password textfield).

--- src/main/java/com/basilisk/frontend/views/LoginView.java
+++ src/main/java/com/basilisk/frontend/views/LoginView.java
@@ -4,12 +4,10 @@ import com.basilisk.backend.presenters.LoginPresenter;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
@@ -19,34 +17,14 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 public class LoginView extends PolymerTemplate<LoginView.LoginViewModel> {
 
     private LoginPresenter loginPresenter;
-    @Id("div")
-    private Element div;
-    @Id("div1")
-    private Element div1;
-    @Id("h2")
-    private H2 h2;
-    @Id("div2")
-    private Element div2;
     @Id("vaadinButton")
-    private Button vaadinButton;
-    @Id("div3")
-    private Element div3;
-    @Id("h21")
-    private H2 h21;
-    @Id("div4")
-    private Element div4;
+    private Button singUpButton;
     @Id("username")
     private TextField username;
     @Id("password")
     private PasswordField password;
     @Id("vaadinButton1")
-    private Button vaadinButton1;
-   /* @Id("div5")
-    private Element div5;
-    @Id("ironIcon")
-    private Element ironIcon;
-    @Id("p")
-    private Paragraph p;*/
+    private Button loginButton;
 
     public LoginView(LoginPresenter loginPresenter) {
         this.loginPresenter = loginPresenter;
